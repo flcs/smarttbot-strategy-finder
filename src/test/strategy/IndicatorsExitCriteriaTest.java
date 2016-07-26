c82d8f681078b4596bd492e94253f775e156814d
Fabio Beider <fabeider@gmail.com>
2016-07-25 21:03:03 -0300

    create test for any indicator exit

--- src/test/strategy/IndicatorsExitCriteriaTest.java
+++ src/test/strategy/IndicatorsExitCriteriaTest.java
@@ -10,11 +10,14 @@ import eu.verdelhan.ta4j.Decimal;
 import eu.verdelhan.ta4j.Order.OrderType;
 import eu.verdelhan.ta4j.TimeSeries;
 import eu.verdelhan.ta4j.Trade;
+import eu.verdelhan.ta4j.indicators.trackers.SMAIndicator;
 import main.core.enums.ExitType;
 import main.core.enums.StopType;
 import main.core.parameters.RobotParameters;
 import main.core.parameters.entry.BollingerBandsParameters;
 import main.core.parameters.entry.EntryParameters;
+import main.core.parameters.entry.MovingAverageParameters;
+import main.core.parameters.entry.RSIParameters;
 import main.core.parameters.exit.ExitParameters;
 import main.core.parameters.exit.FixedStopGainParameters;
 import main.core.parameters.exit.FixedStopLossParameters;
@@ -26,28 +29,18 @@ import test.helpers.TimeSeriesHelper;
 
 public class IndicatorsExitCriteriaTest extends AbstractTest {
 
-	private static TimeSeries buyingSeries;
-	private static TimeSeries sellingSeries;
+	private static TimeSeries timeSeries;
 	private static EntryParameters entryParameters;
 
 	@BeforeClass
 	public static void setupTimeSeries() {
-		int[] highPrices = { 50, 49, 51, 48, 50, 48, 52, 58, 58, 63, 65, 67, 63, 55, 51 };
-		int[] buyingClosePrices = { 50, 51, 49, 52, 51, 52, 49, 43, 42, 41, 38, 35, 41, 51, 50 };
+		int[] closePrices = { 50, 49, 50, 51, 49, 46, 40, 45, 63, 20, 45, 43, 42, 60, 55, 30 };
+		PriceSeries closeSeries = new PriceSeries(PriceType.CLOSE, closePrices);
+		timeSeries = TimeSeriesHelper.getTimeSeries(closeSeries);
 
-		PriceSeries highSeries = new PriceSeries(PriceType.HIGH, highPrices);
-		PriceSeries buyingCloseSeries = new PriceSeries(PriceType.CLOSE, buyingClosePrices);
-		buyingSeries = TimeSeriesHelper.getTimeSeries(highSeries, buyingCloseSeries);
-
-		int[] lowPrices = { 50, 51, 49, 52, 50, 52, 48, 42, 42, 37, 35, 33, 37, 45, 49 };
-		int[] sellingClosePrices = { 50, 49, 51, 48, 49, 48, 51, 57, 58, 59, 62, 65, 59, 49, 50 };
-
-		PriceSeries lowSeries = new PriceSeries(PriceType.LOW, lowPrices);
-		PriceSeries sellingCloseSeries = new PriceSeries(PriceType.CLOSE, sellingClosePrices);
-		sellingSeries = TimeSeriesHelper.getTimeSeries(lowSeries, sellingCloseSeries);
-
-		BollingerBandsParameters bbParam = new BollingerBandsParameters(7, Decimal.TWO);
-		entryParameters = new EntryParameters(null, null, bbParam);
+		MovingAverageParameters sma = new MovingAverageParameters(3, 6);
+		RSIParameters rsi = new RSIParameters(3, 30, 70);
+		entryParameters = new EntryParameters(sma, rsi, null);
 	}
 
 	@Test
@@ -57,79 +50,45 @@ public class IndicatorsExitCriteriaTest extends AbstractTest {
 		RobotParameters parameters = new RobotParameters(entryParameters, exitParameters);
 
 		// Act
-		List<Trade> trades = RobotStrategy.backtest(buyingSeries, parameters);
+		List<Trade> trades = RobotStrategy.backtest(timeSeries, parameters);
 
 		// Assert
 		Trade trade1 = trades.get(0);
-		Assert.assertEquals(7, trade1.getEntry().getIndex());
+		Assert.assertEquals(5, trade1.getEntry().getIndex());
 		Assert.assertEquals(OrderType.BUY, trade1.getEntry().getType());
-		Assert.assertEquals(Decimal.valueOf(43), trade1.getEntry().getPrice());
+		Assert.assertEquals(Decimal.valueOf(46), trade1.getEntry().getPrice());
 
-		Assert.assertEquals(10, trade1.getExit().getIndex());
+		Assert.assertEquals(8, trade1.getExit().getIndex());
 		Assert.assertEquals(OrderType.SELL, trade1.getExit().getType());
-		Assert.assertEquals(Decimal.valueOf("64.07"), trade1.getExit().getPrice());
+		Assert.assertEquals(Decimal.valueOf(63), trade1.getExit().getPrice());
+		
+		Trade trade2 = trades.get(1);
+		Assert.assertEquals(8, trade2.getEntry().getIndex());
+		Assert.assertEquals(OrderType.SELL, trade2.getEntry().getType());
+		Assert.assertEquals(Decimal.valueOf(63), trade2.getEntry().getPrice());
+		
+		Assert.assertEquals(9, trade2.getExit().getIndex());
+		Assert.assertEquals(OrderType.BUY, trade2.getExit().getType());
+		Assert.assertEquals(Decimal.valueOf(20), trade2.getExit().getPrice());
+		
+		Trade trade3 = trades.get(2);
+		Assert.assertEquals(9, trade3.getEntry().getIndex());
+		Assert.assertEquals(OrderType.BUY, trade3.getEntry().getType());
+		Assert.assertEquals(Decimal.valueOf(20), trade3.getEntry().getPrice());
+		
+		Assert.assertEquals(12, trade3.getExit().getIndex());
+		Assert.assertEquals(OrderType.SELL, trade3.getExit().getType());
+		Assert.assertEquals(Decimal.valueOf(42), trade3.getExit().getPrice());
+		
+		Trade trade4 = trades.get(3);
+		Assert.assertEquals(13, trade4.getEntry().getIndex());
+		Assert.assertEquals(OrderType.SELL, trade4.getEntry().getType());
+		Assert.assertEquals(Decimal.valueOf(60), trade4.getEntry().getPrice());
+		
+		Assert.assertEquals(15, trade4.getExit().getIndex());
+		Assert.assertEquals(OrderType.BUY, trade4.getExit().getType());
+		Assert.assertEquals(Decimal.valueOf(30), trade4.getExit().getPrice());
 	}
 
-	@Test
-	public void fixedPercentageStopGainInASellingTrade() {
-		// Arrange
-		FixedStopGainParameters stop = new FixedStopGainParameters(StopType.PERCENTAGE, Decimal.valueOf(37));
-		ExitParameters exitParameters = new ExitParameters(null, null, stop);
-		RobotParameters parameters = new RobotParameters(entryParameters, exitParameters);
-
-		// Act
-		List<Trade> trades = RobotStrategy.backtest(sellingSeries, parameters);
-
-		// Assert
-		Trade trade1 = trades.get(0);
-		Assert.assertEquals(7, trade1.getEntry().getIndex());
-		Assert.assertEquals(OrderType.SELL, trade1.getEntry().getType());
-		Assert.assertEquals(Decimal.valueOf(57), trade1.getEntry().getPrice());
-
-		Assert.assertEquals(10, trade1.getExit().getIndex());
-		Assert.assertEquals(OrderType.BUY, trade1.getExit().getType());
-		Assert.assertEquals(Decimal.valueOf("35.91"), trade1.getExit().getPrice());
-	}
-
-	@Test
-	public void fixedAbsoluteStopGainInABuyingTrade() {
-		// Arrange
-		FixedStopGainParameters stop = new FixedStopGainParameters(StopType.ABSOLUTE, Decimal.valueOf(21));
-		ExitParameters exitParameters = new ExitParameters(null, null, stop);
-		RobotParameters parameters = new RobotParameters(entryParameters, exitParameters);
-
-		// Act
-		List<Trade> trades = RobotStrategy.backtest(buyingSeries, parameters);
-
-		// Assert
-		Trade trade1 = trades.get(0);
-		Assert.assertEquals(7, trade1.getEntry().getIndex());
-		Assert.assertEquals(OrderType.BUY, trade1.getEntry().getType());
-		Assert.assertEquals(Decimal.valueOf(43), trade1.getEntry().getPrice());
-
-		Assert.assertEquals(10, trade1.getExit().getIndex());
-		Assert.assertEquals(OrderType.SELL, trade1.getExit().getType());
-		Assert.assertEquals(Decimal.valueOf(64), trade1.getExit().getPrice());
-	}
-
-	@Test
-	public void fixedAbsoluteStopGainInASellingTrade() {
-		// Arrange
-		FixedStopGainParameters stop = new FixedStopGainParameters(StopType.ABSOLUTE, Decimal.valueOf(21));
-		ExitParameters exitParameters = new ExitParameters(null, null, stop);
-		RobotParameters parameters = new RobotParameters(entryParameters, exitParameters);
-
-		// Act
-		List<Trade> trades = RobotStrategy.backtest(sellingSeries, parameters);
-
-		// Assert
-		Trade trade1 = trades.get(0);
-		Assert.assertEquals(7, trade1.getEntry().getIndex());
-		Assert.assertEquals(OrderType.SELL, trade1.getEntry().getType());
-		Assert.assertEquals(Decimal.valueOf(57), trade1.getEntry().getPrice());
-
-		Assert.assertEquals(10, trade1.getExit().getIndex());
-		Assert.assertEquals(OrderType.BUY, trade1.getExit().getType());
-		Assert.assertEquals(Decimal.valueOf(36), trade1.getExit().getPrice());
-	}
+	
 }
